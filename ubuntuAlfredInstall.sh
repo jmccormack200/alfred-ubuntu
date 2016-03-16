76f0e886a1f3d605b86ecba9bb706f1655e76fca
John McCormack <mccormack.wgsi@gmail.com>
2016-03-16 15:15:46 -0400

    Updated to force yes so no user input is needed

--- ubuntuAlfredInstall.sh
+++ ubuntuAlfredInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #run this to correctly install alfred
 
-sudo apt-get install libgps-dev
-sudo apt-get install libpcap-dev
-sudo apt-get install libcap-dev
+sudo apt-get --yes --force-yes install libgps-dev
+sudo apt-get --yes --force-yes install libpcap-dev
+sudo apt-get --yes --force-yes install libcap-dev
 sudo make CONFIG_ALFRED_CAPABILITIES=n install
